05f658a76b0e0b88995887eac4204aa0ff31d1e2
raveen2k3 <raveen2003@gmail.com>
2022-02-15 21:05:51 +0530

    fixed bot replying for admin coammnds in private

--- zerotwo.py
+++ zerotwo.py
@@ -81,41 +81,48 @@ async def stats (client , message):
 
 @ZeroTwo.on_message(filters.command(["ban"]))
 async def ban (client , message):
-    try:
-        get =await client.get_chat_member(message.chat.id,message.from_user.id) 
-        status = get. status 
-        chat_id = message.chat.id
-        message_id = message.reply_to_message.message_id
-        cmd_user = ["administrator","creator"] 
-        if status in cmd_user:
+    if message.chat.type == "private":
+        await message.reply_text("admins commands can only be used in groups where i am admin with all necessary rights")
+    else:
+        try:
+            get =await client.get_chat_member(message.chat.id,message.from_user.id) 
+            status = get. status 
             chat_id = message.chat.id
-            user_id  = message.reply_to_message.from_user.id
-            await ZeroTwo.ban_chat_member(chat_id, user_id)
-            await message.reply_text(text= "**BANNED SUCCESFULLY**")
-        else:
-            await message.reply_text(text = "** YOU ARE NOT A ADMIN IN THIS CHAT **")
-    except:
-        await message.reply_text("**Error Occured Report in Support Group!!**")
+            message_id = message.reply_to_message.message_id
+            cmd_user = ["administrator","creator"] 
+            if status in cmd_user:
+                chat_id = message.chat.id
+                user_id  = message.reply_to_message.from_user.id
+                await ZeroTwo.ban_chat_member(chat_id, user_id)
+                await message.reply_text(text= "**BANNED SUCCESFULLY**")
+            else:
+                await message.reply_text(text = "** YOU ARE NOT A ADMIN IN THIS CHAT **")
+        except Exception as e:
+            await message.reply_text(e)
 
 
 @ZeroTwo.on_message(filters.command(["unban"]))
 async def unban (client , message):
-    try:
-        get =await client.get_chat_member(message.chat.id,message.from_user.id) 
-        status = get. status 
-        chat_id = message.chat.id
-        message_id = message.reply_to_message.message_id
-        cmd_user = ["administrator","creator"] 
-        if status in cmd_user:
+    if message.chat.type == "private":
+        await message.reply_text("admins commands can only be used in groups where i am admin with all necessary rights")
+    else:
+        
+        try:
+            get =await client.get_chat_member(message.chat.id,message.from_user.id) 
+            status = get. status 
             chat_id = message.chat.id
-            user_id  = message.reply_to_message.from_user.id
-            await ZeroTwo.unban_chat_member(chat_id, user_id)
-            await message.reply_text(text= "**UNBANNED SUCCESFULLY**")
-        else:
-            await message.reply_text(text = "** YOU ARE NOT A ADMIN IN THIS CHAT **")
-    except:
-        await message.reply_text("**Error Occured Report in Support Group!!**")
-
+            message_id = message.reply_to_message.message_id
+            cmd_user = ["administrator","creator"] 
+            if status in cmd_user:
+                chat_id = message.chat.id
+                user_id  = message.reply_to_message.from_user.id
+                await ZeroTwo.unban_chat_member(chat_id, user_id)
+                await message.reply_text(text= "**UNBANNED SUCCESFULLY**")
+            else:
+                await message.reply_text(text = "** YOU ARE NOT A ADMIN IN THIS CHAT **")
+        except Exception as e:
+            await message.reply_text(e)
+    
 
 
 
@@ -123,76 +130,65 @@ async def unban (client , message):
 
 @ZeroTwo.on_message(filters.command(["promote"]))
 async def promote (client , message):
-    try:
-        get =await client.get_chat_member(message.chat.id,message.from_user.id) 
-        status = get. status 
-        chat_id = message.chat.id
-        message_id = message.reply_to_message.message_id
-        cmd_user = ["administrator","creator"] 
-        if status in cmd_user:
-            user_id = message.reply_to_message.from_user.id
-            await ZeroTwo.promote_chat_member(chat_id, user_id)
-            await ZeroTwo.send_message(chat_id, "promoted successfully")
-        else:
-            await message.reply_text("You don't have enough rights!!")
-    except:
-        await message.reply_text("ERROR OCCURED TRY CONTACTING OUR SUPPORT GROUP")
+    if message.chat.type == "private":
+        await message.reply_text("admins commands can only be used in groups where i am admin with all necessary rights")
+    else:
+        try:
+            get =await client.get_chat_member(message.chat.id,message.from_user.id) 
+            status = get. status 
+            chat_id = message.chat.id
+            message_id = message.reply_to_message.message_id
+            cmd_user = ["administrator","creator"] 
+            if status in cmd_user:
+                user_id = message.reply_to_message.from_user.id
+                await ZeroTwo.promote_chat_member(chat_id, user_id)
+                await ZeroTwo.send_message(chat_id, "promoted successfully")
+            else:
+                await message.reply_text("You/I don't have enough rights!!")
+        except Exception as e:
+            await message.reply_text(e)
 
 @ZeroTwo.on_message(filters.command(["pin"]))
 async def pin (client , message):
-  try:
-      get =await client.get_chat_member(message.chat.id,message.from_user.id) 
-      status = get. status 
-      chat_id = message.chat.id
-      message_id = message.reply_to_message.message_id
-      cmd_user = ["administrator","creator"] 
-      if status in cmd_user:
-          await ZeroTwo.pin_chat_message(chat_id, message_id)
-          await message.reply_text(text = "Pinned to Top🥺")
-      
-      else:
-          await message.reply_text('Only Admin Can Pin Messages')
-  except:
-        await message.reply_text("REPLY TO A MESSAGE TO PIN IT ")
+    if message.chat.type == "private":
+        await message.reply_text("admins commands can only be used in groups where i am admin with all necessary rights")
+    else:
+            try:
+                get =await client.get_chat_member(message.chat.id,message.from_user.id) 
+                status = get. status 
+                chat_id = message.chat.id
+                message_id = message.reply_to_message.message_id
+                cmd_user = ["administrator","creator"] 
+                if status in cmd_user:
+                    await ZeroTwo.pin_chat_message(chat_id, message_id)
+                    await message.reply_text(text = "Pinned to Top🥺")
+                
+                else:
+                    await message.reply_text('Only Admin Can Pin Messages')
+            except Exception as e:
+                    await message.reply_text(e)
 
 @ZeroTwo.on_message(filters.command(["admintitle"]))
 async def adminTITLE(client , message):
-    chat_id = message.chat.id
-    get =await client.get_chat_member(message.chat.id,message.from_user.id) 
-    status = get.status
-    cmd_user = ["administrator","creator"]
-    msg = message.text
-    title = msg.split(' ')[1]
-    user_id = message.reply_to_message.from_user.id
-    try:
-        if status in cmd_user:
-            await ZeroTwo.set_administrator_title(chat_id, user_id, title)
-            await message.reply_text("title updated")
-        else:
-            await ZeroTwo.send_message(chat_id, "you dont have enough rights , to make changes!!")
-    except:
-            await message.reply_text("ERROR OCCURED TRY CONTACTING OUR SUPPORT GROUP")
-
-
-@ZeroTwo.on_message(filters.command(["eval"]))
-async def eval (client , message):
-    chat_id = message.chat.id
-    inputgot = message.text[0]
-    string = subprocess.run([inputgot], capture_output=True)
-    await message.reply(text = string)
-
-
-
-@ZeroTwo.on_message(filters.command(["adminlist"]))
-async def adminlist (client , message):
-    chat_id = message.chat.id
-    chat_name = message.chat.username
-    async for member in ZeroTwo.iter_chat_members(chat_name, filter="administrators"):
-        print(member.user.first_name + ' is admin!')
-        await message.reply_text(text = member.user.first_name + ' is admin!')
-
-
-
+    if message.chat.type == "private":
+        await message.reply_text("admins commands can only be used in groups where i am admin with all necessary rights")
+    else:
+    
+        chat_id = message.chat.id
+        get =await client.get_chat_member(message.chat.id,message.from_user.id) 
+        status = get.status
+        cmd_user = ["administrator","creator"]
+        msg = message.text
+        title = msg.split(' ')[1]
+        user_id = message.reply_to_message.from_user.id
+        try:
+            if status in cmd_user:
+                await ZeroTwo.set_administrator_title(chat_id, user_id, title)
+                await message.reply_text("title updated")
+            else:
+                await ZeroTwo.send_message(chat_id, "you dont have enough rights , to make changes!!")
+        except Exception as e:
+                    await message.reply_text(e)
 
 
 
